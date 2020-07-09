09b20e5642bab1afcf448e8676f0df40a27ae298
Manuel Álvarez Díaz <manuel.alvarez@udc.es>
2020-07-09 23:37:31 +0200

    MySQLCreateData: Añadidas passwords encriptadas

--- backend/src/sql/2-MySQLCreateData.sql
+++ backend/src/sql/2-MySQLCreateData.sql
@@ -15,9 +15,9 @@ INSERT INTO City (name) VALUES ('Sevilla');
 INSERT INTO City (name) VALUES ('Ferrol');
 
 INSERT INTO User (userName, password, firstName, lastName, email, phone, role)
-	VALUES ('client', 'client', 'name', 'lastname', 'client@client.com', 132456798, 0); 
+	VALUES ('client', '$2a$10$aZ7xTAvxVTqnrdbeXB.HJujBgVfjeCzvPikU/YyFYxtXUOSgMu67i', 'name', 'lastname', 'client@client.com', 132456798, 0); 
 INSERT INTO User (userName, password, firstName, lastName, email, phone, role)
-	VALUES ('business', 'business', 'nameb', 'lastnameb', 'business@business.com', 345176824, 1); 
+	VALUES ('business', '$2a$10$B6DS8oiZ0fUXNC7HlNRe0.0FpNTWstct2SEujaJYX83Abupq6ptOO', 'nameb', 'lastnameb', 'business@business.com', 345176824, 1); 
 	
 INSERT INTO Company (name, capacity, reserve, homeSale, reservePercentage, userId, companyCategoryId, block)
 	VALUES ('Delivr', 25, true, true, 20, 2, 3, false);
